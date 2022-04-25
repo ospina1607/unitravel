e946a6783f2b1af0235e71dbfbafe0e82492654a
ospina1607 <ospina0212311@gmail.com>
2022-04-25 18:01:28 -0500

    Primera version

---
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/PersistenciaApp.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.unitravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaApp {
+
+
+    public static void main(String [] args){
+        SpringApplication.run(PersistenciaApp.class ,args);
+    }
+}
